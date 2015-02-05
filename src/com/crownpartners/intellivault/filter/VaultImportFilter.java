32181f9694f12a72b29454b5a35573fc1335787d
Sean Steimer <ssteimer@crownpartners.com>
2015-02-04 18:27:05 -0600

    Fixing issue with call to String.format for logging

--- src/com/crownpartners/intellivault/filter/VaultImportFilter.java
+++ src/com/crownpartners/intellivault/filter/VaultImportFilter.java
@@ -7,11 +7,8 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 /**
- * Created with IntelliJ IDEA.
- * User: sean.steimer
- * Date: 4/3/13
- * Time: 2:05 PM
- * To change this template use File | Settings | File Templates.
+ * Created with IntelliJ IDEA. User: sean.steimer Date: 4/3/13 Time: 2:05 PM To change this template use File | Settings
+ * | File Templates.
  */
 public class VaultImportFilter implements Filter<VirtualFile> {
     private List<String> ignorePatterns;
@@ -20,20 +17,21 @@ public class VaultImportFilter implements Filter<VirtualFile> {
 
     /**
      * Create a new VaultImportFilter instance based on a List of ignore patterns
+     *
      * @param ignorePatterns the ignore patterns for this filter
      */
     public VaultImportFilter(List<String> ignorePatterns) {
-        this.ignorePatterns=ignorePatterns;
+        this.ignorePatterns = ignorePatterns;
     }
 
     @Override
     public boolean allows(VirtualFile file) {
-        for(String ignorePattern : ignorePatterns) {
+        for (String ignorePattern : ignorePatterns) {
             boolean matches = file.getName().equals(ignorePattern) ||
                     Pattern.compile(ignorePattern).matcher(file.getName()).matches();
-            if(matches) {
+            if (matches) {
                 log.info(String.format("Skipping file %s, matched ignore pattern %s.",
-                        new String[]{file.getCanonicalPath(),ignorePattern}));
+                        file.getCanonicalPath(), ignorePattern));
                 return false;
             }
         }
