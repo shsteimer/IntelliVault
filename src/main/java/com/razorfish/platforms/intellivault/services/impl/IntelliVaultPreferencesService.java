b4f49a12f7b9ca2e44e939b93796f4f4d4ed605e
Sean Steimer <ssteimer@adobe.com>
2020-03-18 18:18:26 -0700

    work in progress

--- src/main/java/com/razorfish/platforms/intellivault/services/impl/IntelliVaultPreferencesService.java
+++ src/main/java/com/razorfish/platforms/intellivault/services/impl/IntelliVaultPreferencesService.java
@@ -1,16 +1,23 @@
 package com.razorfish.platforms.intellivault.services.impl;
 
+import com.intellij.credentialStore.CredentialAttributes;
+import com.intellij.credentialStore.CredentialAttributesKt;
+import com.intellij.credentialStore.Credentials;
+import com.intellij.ide.passwordSafe.PasswordSafe;
 import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
+import com.razorfish.platforms.intellivault.config.IntelliVaultCRXRepository;
 import com.razorfish.platforms.intellivault.config.IntelliVaultPreferences;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 /**
  * The preferences services handles storing and retrieving the configuration state of the vault plugin.
  */
 @State(name = "IntelliVaultPreferencesService", storages = {
-        @Storage("IntelliVaultPreferencesService.xml") })
+        @Storage("IntelliVaultPreferencesService.xml")})
 public class IntelliVaultPreferencesService implements PersistentStateComponent<IntelliVaultPreferences> {
 
     private IntelliVaultPreferences preferences;
@@ -34,11 +41,44 @@ public class IntelliVaultPreferencesService implements PersistentStateComponent<
     @Nullable
     @Override
     public IntelliVaultPreferences getState() {
-        return getPreferences();
+        IntelliVaultPreferences preferences = getPreferences();
+
+        List<IntelliVaultCRXRepository> repoConfigList = preferences.getRepoConfigList();
+        repoConfigList.forEach(repo -> {
+            CredentialAttributes credentialAttributes = createCredentialAttributes(repo.getName());
+            Credentials credentials = new Credentials(repo.getUsername(), repo.getPassword());
+            PasswordSafe.getInstance().set(credentialAttributes, credentials);
+
+            repo.setPassword("dummy");
+            repo.setUsername("dummy");
+        });
+
+
+        return preferences;
+
     }
 
     @Override
     public void loadState(IntelliVaultPreferences preferences) {
+
+        List<IntelliVaultCRXRepository> repoConfigList = preferences.getRepoConfigList();
+        repoConfigList.forEach(repo -> {
+            CredentialAttributes credentialAttributes = createCredentialAttributes(repo.getName());
+
+            Credentials credentials = PasswordSafe.getInstance().get(credentialAttributes);
+            if (credentials != null) {
+                String password = credentials.getPasswordAsString();
+                String userName = credentials.getUserName();
+
+                repo.setPassword(password);
+                repo.setUsername(userName);
+            }
+        });
+
         setPreferences(preferences);
+    }
+
+    private CredentialAttributes createCredentialAttributes(String key) {
+        return new CredentialAttributes(CredentialAttributesKt.generateServiceName("IntelliVault", key));
     }
 }
