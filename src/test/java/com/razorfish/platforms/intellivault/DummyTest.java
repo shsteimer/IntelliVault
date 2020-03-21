f083d345c8af6ab04113e5a15137297bed2ea02c
Sean Steimer <ssteimer@adobe.com>
2020-03-20 23:51:56 -0700

    added test basics

---
+++ src/test/java/com/razorfish/platforms/intellivault/DummyTest.java
@@ -0,0 +1,13 @@
+package com.razorfish.platforms.intellivault;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class DummyTest {
+
+    @Test
+    public void testNothing() throws Exception {
+        assertTrue(true);
+    }
+}
